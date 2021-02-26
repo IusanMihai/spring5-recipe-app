8c64f47a2650419fa7af2a448543e1ea477f3e16
mihai <mihai.iusan@gmail.com>
2021-02-26 16:33:40 +0100

    Added Project Lombok to Utilize get rid of boilerplate code (Getter and Setters, Equals, HashCode, ToString)

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -5,12 +5,14 @@ import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -31,6 +33,8 @@ public class IndexController {
 
         System.out.println("Cat id is: " + categoryOptional.get().getId());
         System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
